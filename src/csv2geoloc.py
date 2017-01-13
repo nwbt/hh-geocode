1476d51f6fd8c0e848db7e2e24f28dfb14485bd1
Dan Catalano <dev@nwbt.co>
2017-01-13 16:29:44 -0700

    store object cleam up

--- src/csv2geoloc.py
+++ src/csv2geoloc.py
@@ -7,6 +7,8 @@
 # Distributed under terms of the MIT license.
 
 from collections import OrderedDict
+from googlemaps import geocoding
+from googlemaps import client
 import csv
 import sys
 import os
@@ -18,45 +20,63 @@ import json
 logfile = 'logs/csvToJson.txt'
 loglevel = 'WARNING'
 logmsg = '%(asctime)s|%(levelname)s|%(message)s'
+gkey = ''
 
 class Store:
 
     def __init__(this, store_dict):
-        this.store_dict = store_dict
+        this.id = store_dict['id']
+        this.store = store_dict['store']
+        this.address = store_dict['address']
+        this.address2 = store_dict['address2']
+        this.city = store_dict['city']
+        this.state = store_dict['state']
+        this.zip = store_dict['zip']
+        this.country = store_dict['country']
+        this.latitude = store_dict['latitude']
+        this.longitude = store_dict['longitude']
+        this.tags = store_dict['tags']
+        this.email = store_dict['email']
+        this.url = store_dict['url']
+        this.hours = store_dict['hours']
+        this.phone = store_dict['phone']
+        this.fax = store_dict['fax']
+        this.image = store_dict['image']
+        this.private = store_dict['private']
+        this.neat_title = store_dict['neat_title']
+        this.description = store_dict['description']
 
+    def full_address(this):
+        return this.store 
 
-class CSVFile:
+def build_store_list_from_csv(filename):
+    
+    store_list = []
 
-    def __init__(this, filename):
+    try:
+        with open(filename) as csvfile:
+            dialect = csv.Sniffer().sniff(csvfile.read(1024))
+            csvfile.seek(0)
+            try:
+                reader = csv.DictReader(csvfile, dialect=dialect)
+                for idx, row in enumerate(reader):
+                    newrow = _rekey(row) 
+                    store_list.append(Store(newrow))
 
-        this.filename = filename
-        this._create_store_list()
+            except IOError as ioe:
+                logging.critical(ioe.strerror)
+                return
 
-    def _create_store_list(this):
-        
-        this.store_list = []
+    except FileNotFoundError as fnfe:
+        logging.critical(fnfe.strerror + ",filename:" + fnfe.filename)
+        return
 
-        try:
-            with open(this.filename) as csvfile:
-                dialect = csv.Sniffer().sniff(csvfile.read(1024))
-                csvfile.seek(0)
-                try:
-                    reader = csv.DictReader(csvfile, dialect=dialect)
-                    for idx, row in enumerate(reader):
-                        newrow = _rekey(row) 
-                        this.store_list.append(newrow)
-
-                except IOError as ioe:
-                    logging.critical(ioe.strerror)
-
-        except FileNotFoundError as fnfe:
-            logging.critical(fnfe.strerror + ",filename:" + fnfe.filename)
-
+    return store_list
 
 def _rekey(row):
 
     exp = re.compile(r"""(^sl_)(.*$)""")
-    rekeyed_row = OrderedDict()
+    rekeyed_row = {} 
     for key in row:
         match = exp.search(key)
         rekeyed_row[match.group(2)] = row[key]
@@ -64,26 +84,16 @@ def _rekey(row):
     return rekeyed_row
 
 
-class JSONFile:
-
-    def __init__(this, filename):
-
-        this.filename = filename
-
-
-def open_and_read_file(filename):
-    pass
-    # todo log error and 
-
 @click.command()
 @click.argument('inputfile')
+@click.argument('google_api_key')
 @click.option('--outputfile', '-o', default='resources/geolocations.json')
 @click.option('--loglevel', '-l', default=loglevel)
 @click.option('--logfile', '-f', default=logfile)
-def csv_to_json(inputfile, outputfile, loglevel, logfile):
+def csv_to_json(inputfile, google_api_key, outputfile, loglevel, logfile):
 
     logging.basicConfig(filename=logfile, level=loglevel.upper(), format=logmsg)
-    csvfile = CSVFile(inputfile)
+    store_list = build_store_list_from_csv(inputfile)
     pass
 
 if __name__ == '__main__':
